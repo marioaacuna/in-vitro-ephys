44f9c3beb712ae1a01662a8a09e2cfb042edc62f
Liselot <lotte.spierenburg@unibe.ch>
2021-04-29 11:58:17 +0200

    Changed root path for initialization

--- RUN.m
+++ RUN.m
@@ -1,4 +1,4 @@
 close all force hidden; clc
 clear,
-cd('M:\Mario\Fede\Ephys_in_vitro_analysis') % To be changed later (add in repository)
+cd('C:\Repositories\in-vitro-ephys') % To be changed later (add in repository)
 run_in_vitro_ephys_analysis()
