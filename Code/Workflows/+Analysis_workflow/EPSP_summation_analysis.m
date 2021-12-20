98f6e3394a6a87c117f280d302e1e1703bf5d973
Mario <mario.acuna@unibe.ch>
2021-12-20 14:17:25 +0100

    Created algorithms to estimate values of EPSP summation analysis

---
+++ Code/Workflows/+Analysis_workflow/EPSP_summation_analysis.m
@@ -0,0 +1,428 @@
+function varargout = EPSP_summation_analysis(~,V_traces, I_traces, do_plotting, name, p)
+% Parse inputs
+SR = p{1};
+start_stim = p{2} / 1000;
+testpulse_start = p{3} / 1000;
+if ~isempty(p{5})
+    testpulse_duration = p{5} / 1000;
+end
+testpulse_amplitude = p{4}; % in pA
+total_duration = p{6};
+win_to_pk = p{7};
+global GC
+%% Find peaks
+warning('off')
+% sampling_freq = 8000;
+% start_time = 0.100;
+% end_time = 0.7;
+n_sweeps= size(V_traces,2);
+
+% Current traces info
+current_steps = size(I_traces,2);
+%p{5} = nr of sweeps
+%p{6} = inter-sweep interval
+pulses = NaN(1, current_steps);
+% is_paring_trial = length(n_pulses) == 3; % baseline, AP and hyperpol current
+
+
+if   p{1} ~= ((size(I_traces,1)) / total_duration) 
+    disp (['Experiment: ', name, ' wrong parameters'])
+    return
+end
+
+
+
+
+
+% 
+% 
+% 
+% if ~is_paring_trial
+%     for i_p = 1 : current_steps
+%         this_pulse_points = unique(I_traces(:,i_p));
+%         is_0 = sum(this_pulse_points) == 0;
+%         if ~is_0
+%             pulses(i_p) = this_pulse_points(this_pulse_points ~= 0);
+%         else
+%             pulses(i_p) = 0;
+%         end
+%     end
+%     current_duration = (sum(I_traces == pulses)-1) / SR;
+%     this_total_duration = (size(I_traces,1)) / SR;
+%     if current_duration ~= testpulse_duration || p{4} ~= unique(pulses) || p{1} ~= ((size(I_traces,1)) / total_duration) || this_total_duration ~= total_duration
+%         disp (['Experiment: ', name, ' has different number of sweeps or different frequency, PLEASE SELECT DIFFERENT PARAMETERS'])
+%         return
+%     end
+%     endpoint = 0.1 * SR; % 100 ms
+%     min_distance = 0.005;
+%     min_width = 10 / SR;
+%     MinPeakProminence = 0.2;
+%     MinPeakHeight =  0.5;
+% else
+%     % Set different parameters for AP detection 
+%     disp(['Experiment: ', name, ' is a pairing trial'])
+%     [~, max_idx] = max(n_pulses);
+%     start_stim = find(I_traces, n_pulses(max_idx), 'first');
+%     start_stim = (start_stim(1) - 1)   /SR;
+%     endpoint = 0.250 * SR; % 250ms
+%     min_distance = 0.005;
+%     min_width = 10 / SR;
+%     MinPeakProminence = 20;
+%     MinPeakHeight =  20;
+%     
+%     % Pasive membrane params
+%     [~, min_idx] = min(n_pulses);
+%     length_test_pulse = length(find(ismember(I_traces, n_pulses(min_idx)))) -1;
+%     testpulse_duration = length_test_pulse / SR;
+%     testpulse_start = find( ismember(I_traces, n_pulses(min_idx)), 1, 'first');
+%     testpulse_start = (testpulse_start - 1) / SR;
+% end
+
+
+%% Set variables to consider for eact sweep
+Vm = NaN(n_sweeps,1);
+Area = Vm;
+% EPSP parameters
+Amp = Vm;
+Ratio = Vm;
+Rise_time = Vm;
+Decay_time = Vm;
+Slope = Vm;
+Width = Vm;
+Onset = Vm; % to be calculated
+Sweep_ids = Vm;
+
+% Peak detection parameters
+endpoint = 0.1 * SR; % 100 ms
+min_distance = 0.020;
+min_width = 0.020;
+MinPeakProminence = 1.5;
+MinPeakHeight =  1.5;
+
+% detect if this data based on only one epsp or more
+this_data = I_traces(:,2);
+this_data_smoothed = smooth(this_data, SR * 0.001);
+bsl = this_data_smoothed(1 :start_stim*SR-1 );
+median_bsl= median(bsl);
+this_EPSP_short = this_data_smoothed(start_stim*SR +1 : end) - median_bsl;
+%     data_start_stim = this_EPSP_short(start_stim*SR :end);
+data_start_stim = this_EPSP_short;
+x = (1:length(data_start_stim)) ./ SR;
+[I_peaks, I_loc, I_w] = findpeaks(data_start_stim, x, 'MinPeakProminence', 0,...
+    'MinPeakHeight',0, 'MinPeakDistance', 0, 'MinPeakWidth', 0);
+n_peaks = length(I_peaks);
+
+
+%% detect the peaks of each stimulation
+I_total_peaks = NaN(n_sweeps, n_peaks);
+for is = 1:n_sweeps
+    
+    this_data = I_traces(:,is);
+    this_data_smoothed = smooth(this_data, SR * 0.001);
+    bsl = this_data_smoothed(1 :start_stim*SR-1 );
+    median_bsl= median(bsl);
+    this_EPSP_short = this_data_smoothed(start_stim*SR +1 : end) - median_bsl;
+    %     data_start_stim = this_EPSP_short(start_stim*SR :end);
+    data_start_stim = this_EPSP_short;
+    x = (1:length(data_start_stim)) ./ SR;
+    [I_peaks, I_loc, I_w] = findpeaks(data_start_stim, x, 'MinPeakProminence', 0,...
+        'MinPeakHeight',0, 'MinPeakDistance', 0, 'MinPeakWidth', 0);
+    if ~isempty(I_peaks)
+        I_total_peaks(is, :) = round(I_peaks)';
+    else
+         I_total_peaks(is, :) = zeros(1,n_peaks);
+    end
+    %     n_peaks = length(I_peaks);
+     
+end
+
+%%
+Amp = NaN(n_sweeps, n_peaks);
+Ratio =  NaN(n_sweeps,1);
+if n_peaks == 1
+     is_only_1_protocol = 1;
+     Amp = NaN(n_sweeps, n_peaks);
+     Ratio = Vm;
+
+elseif n_peaks == 0
+%       keyboard
+    
+else
+    
+%     keyboard
+    is_only_1_protocol = 0;
+    n_peaks_this_protocol = length(I_loc);
+    peaks_loc = I_loc;
+end
+
+%%
+if is_only_1_protocol
+    
+    hold on
+    for i_data  = 1: n_sweeps
+        
+        % Detect Peaks voltage trace
+        this_data = V_traces(:,i_data);
+        this_data_smoothed = smooth(this_data, SR * 0.001);
+        bsl = this_data_smoothed(1 :start_stim*SR-1 );
+        median_bsl= median(bsl);
+        this_EPSP_short = this_data_smoothed(start_stim*SR +1 : start_stim*SR +1 + 0.1*SR) - median_bsl;
+        %     data_start_stim = this_EPSP_short(start_stim*SR :end);
+        data_start_stim = this_EPSP_short;
+        x = (1:length(data_start_stim)) ./ SR;
+        [peaks, loc, w] = findpeaks(data_start_stim, x, 'MinPeakProminence', MinPeakProminence,...
+            'MinPeakHeight',MinPeakHeight, 'MinPeakDistance', min_distance, 'MinPeakWidth', min_width);
+        
+        if isempty(peaks) %|| loc(1) > 0.015 % if there's no peak or if the first peak is found later than 15ms
+            this_amp = NaN;
+            loc = NaN;
+            w = NaN;
+            this_area = NaN;
+            this_ratio = NaN;
+            Sweep_ids(i_data) = i_data;
+            continue
+            %         [~, peak_to_decay] = max(max_10);
+            %         loc = loc_max10 / SR;
+            %         w = NaN;
+            %     else
+            %
+            %         [~, peak_to_decay] = max(peaks);
+        end
+        
+        this_amp = peaks;
+        
+        
+        F_derivative = gradient(smooth(this_EPSP_short, 100));
+        
+        % Get 0-crossings
+        zx = find(sign(F_derivative(1:end-1).*F_derivative(2:end)) < 0);
+        % Remove spurious points
+        zx(zx<1) = [];
+        zx(zx>=length(F_derivative)) = [];
+        yx = [F_derivative(zx) F_derivative(zx+1)];
+        % Keep transitions from rising to falling
+        pos_zx = zx(yx(:,1)>=0 & yx(:,2)<0);
+        
+        
+        epsp_to_area = this_EPSP_short(1:pos_zx(3));
+        this_area = sum(epsp_to_area); %trapz(epsp_to_area)
+        
+        if do_plotting
+            cla
+            plot(x, data_start_stim, 'k')
+            hold on
+            plot(loc, this_amp,  'r*')
+%             plot(f,x_d,y)
+            legend off
+            title([name, ' ; sweep ', num2str(i_data)])
+            pause(0.1)
+            
+        end
+        
+        % Output parameters
+        Vm(i_data) = median_bsl;
+        Area (i_data)= this_area;
+        % EPSP parameters
+        Amp(i_data) = this_amp;
+        Ratio(i_data) = this_amp/this_amp;  
+        Rise_time(i_data) = [];%rise_time;
+        Decay_time(i_data) = [];%decay_time;
+        Slope(i_data) =[]; %slope;
+        Width(i_data) = w;
+        Onset(i_data) = [];%this_onset; % to be calculated
+        Sweep_ids(i_data) = i_data;
+        
+    end
+    
+    disp('only one injection per sweep')
+else
+    hold on
+    for i_data  = 1: n_sweeps
+        
+        
+        % Detect max value within the first 10ms or given by the user
+        %     x_10 = (win_to_pk / 1000) * SR;
+        %     [max_10, loc_max10] = max(data_start_stim(1:x_10));
+        %     loc_max10 = loc_max10;
+        
+        
+        % Detect Peaks voltage trace
+        this_data = V_traces(:,i_data);
+        this_data_smoothed = smooth(this_data, SR * 0.001);
+        bsl = this_data_smoothed(1 :start_stim*SR-1 );
+        median_bsl= median(bsl);
+        this_EPSP_short = this_data_smoothed(start_stim*SR +1 : end) - median_bsl;
+        %     data_start_stim = this_EPSP_short(start_stim*SR :end);
+        data_start_stim = this_EPSP_short;
+        x = (1:length(data_start_stim)) ./ SR;
+        
+        % set if there are peaks
+        if sum(I_total_peaks(i_data,:)) == 0
+            p = [];
+        else
+            p = 1;
+        end
+        if ~isempty(p)
+            
+            x_peaks = I_loc * SR;
+            peaks = [];
+            loc = [];
+            w=[];
+            for ip = 1:n_peaks_this_protocol
+                [this_max, this_loc] =max(data_start_stim(x_peaks(ip)-(0.0015*SR): x_peaks(ip) + (0.0150 *SR)));
+                this_loc = this_loc + x_peaks(ip) - (0.0016*SR);
+                peaks(ip) = this_max;
+                loc(ip) = this_loc;   
+            end
+            
+        else %if isempty(peaks) %|| loc(1) > 0.015 % if there's no peak or if the first peak is found later than 15ms
+            this_amp = NaN(size(I_total_peaks));
+            loc = NaN(size(I_total_peaks));
+            w = NaN(size(I_total_peaks));
+            this_area = NaN;
+            Sweep_ids (i_data) = i_data;
+            
+            
+            if do_plotting
+                cla
+                plot(x, data_start_stim, 'k')
+                hold on
+                plot(loc, this_amp,  'r*')
+%                 plot(f,x_d,y)
+                legend off
+                title([name, ' ; sweep ', num2str(i_data)])
+                pause(0.1)
+                
+            end
+            
+            continue
+        end        
+        % stop if an AP was detected
+        is_AP = (peaks > 30);
+        if any(is_AP)
+            varargout{1} = Sweep_ids;
+            varargout{2} = Amp;
+            varargout{3} = I_total_peaks;
+            varargout{4} = [];
+            varargout{5} = [];
+            varargout{6} = [];
+            varargout{7} = Ratio;
+            varargout{8} = Vm;
+            varargout{9} = Area;
+             disp([num2str(n_peaks_this_protocol),' injections per sweep'])
+            return
+            continue
+        end
+        
+        this_amp = peaks;
+        
+        
+        F_derivative = gradient(smooth(this_EPSP_short, 100));
+        
+        % Get 0-crossings
+        zx = find(sign(F_derivative(1:end-1).*F_derivative(2:end)) < 0);
+        % Remove spurious points
+        zx(zx<1) = [];
+        zx(zx>=length(F_derivative)) = [];
+        yx = [F_derivative(zx) F_derivative(zx+1)];
+        % Keep transitions from rising to falling
+        pos_zx = zx(yx(:,1)>=0 & yx(:,2)<0);
+        
+        idx_to_take = 2*n_peaks_this_protocol -1;
+        epsp_to_area = this_EPSP_short(1:pos_zx(idx_to_take));
+        this_area = sum(epsp_to_area); %trapz(epsp_to_area)
+        
+        
+        
+        
+        %     this_amp = peaks;
+        % %     this_amp = peaks(peak_to_decay);
+        %     % Calculate rise time
+        %     try % it could be that there are no EPSPs detected and then it might
+        %         rise_time = 1000 * risetime(data_start_stim(1:ceil(loc_max10(1))), SR); % It calculates the rise time to the first peak (during the fist 10ms), if found > 1; in ms
+        %     catch
+        % %         keyboard
+        %         rise_time = NaN;
+        %     end
+        %     % Calculate decay time
+        %     if length(rise_time) > 1, rise_time = NaN;end
+        %     y = data_start_stim(loc(peak_to_decay)*SR:end); % Values of voltages from the peak to the end
+        %     x_d = x(ceil(loc(peak_to_decay) *SR) :end);
+        %     f = fit(x_d',y,'exp1');
+        %     decay_time = abs(f.b); % in ms ?
+        %
+        %     % calculate Slope
+        % %     above0 = find(data_start_stim > 0,1, 'first');
+        %     percent_i = this_amp(1) * 0.2;
+        %     percent_f = this_amp(1) * 0.8;
+        %     [~,xi] = min(abs(percent_i-data_start_stim(1: loc(peak_to_decay)*SR)));
+        %     [~, xf] =  min(abs(percent_f-data_start_stim(1: loc(peak_to_decay)*SR)));
+        %
+        %     dx = xf - xi;
+        %     if dx ~= 0 % happenes when the points are very close together when there's no EPSP
+        % %     dx = x(loc(1)*SR) - x(above0);
+        %         slope = unique(gradient([data_start_stim(xi),data_start_stim(xf)]  ,dx/SR*1000));
+        %     else
+        %         slope = NaN;
+        %     end
+        %     % Get the width
+        %     this_width = w(peak_to_decay) * 1000;
+        if do_plotting
+            cla
+            plot(x, data_start_stim, 'k')
+            hold on
+            plot(loc/SR, this_amp,  'r*')
+%             plot(f,x_d,y)
+            legend off
+            title([name, ' ; sweep ', num2str(i_data)])
+            pause(0.1)
+            
+        end
+        %     % calculate Ri
+        %     ss = median(this_data_smoothed(testpulse_start * SR + 0.1*SR : testpulse_start * SR + testpulse_duration * SR));
+        %     this_R = abs(((ss - median_bsl) *10^-3) / (testpulse_amplitude*10^-9)) /1000; % in MOhms
+        %
+        %     % Calculate onset
+        %     % This calculation takes the 4*std from the baseline value of the trace
+        %     % and subtracts it to the stimulus onset.
+        %     % verify this calculation with other datasets, because the arctifact can be different
+        %     thr = 4 * std(bsl);
+        %     onset_idx = find(data_start_stim > thr, 1, 'first');
+        %     if isempty (onset_idx) % it could be that there is activity before, increasing the std
+        %         onset_idx = xi;
+        %     end
+        %     this_onset = 1000*((start_stim + onset_idx / SR) - start_stim); % ms
+        
+        % Output parameters
+        Vm(i_data) = median_bsl;
+        Area (i_data)= this_area;
+        % EPSP parameters
+        Amp(i_data,:) = this_amp;
+        Ratio(i_data) = this_amp(end) / this_amp(1); 
+        Rise_time(i_data) = [];%rise_time;
+%         Decay_time(i_data) = [];%decay_time;
+%         Slope(i_data) =[]; %slope;
+%         Width(i_data) = [];
+        Onset(i_data) = [];%this_onset; % to be calculated
+        Sweep_ids(i_data) = i_data;
+    end
+   
+end
+
+
+
+
+
+% Vm = mean(all_Vms);
+%% Output arguments
+disp(['good cell : ', name])
+varargout{1} = Sweep_ids;
+varargout{2} = Amp;
+varargout{3} = I_total_peaks;
+varargout{4} = [];
+varargout{5} = [];
+varargout{6} = [];
+varargout{7} = Ratio;
+varargout{8} = Vm;
+varargout{9} = Area;
+
---
+++ run_Summation.m
@@ -0,0 +1,269 @@
+function DONE =  run_Summation(experimenter_ID, recording_date, do_plotting, varargin)
+clc
+DONE = 0;
+p = varargin;
+overwrite = p{8};
+% Set global variables
+global GC
+% Read general_configs
+GC = general_configs();
+%%
+GC.experimenter_ID = experimenter_ID;
+data_path_root =  GC.raw_data_root_path.(experimenter_ID); % This needs to be added on GC
+toolboxes_to_use = {'Igor2Matlab'};
+toggle_toolbox(toolboxes_to_use, 'on')
+switch experimenter_ID
+    case 'Federica'
+        data_path = os.path.join(data_path_root, recording_date{1}, 'traces');
+        unique_datapath = 1;
+        
+    case 'Liselot'
+        data_path = os.path.join(data_path_root, '2. opto-5HT7', recording_date{1}, [recording_date{1},'.data']);
+        unique_datapath = 1;
+    case 'Kristina'
+        initial_path = os.path.join(data_path_root, recording_date{1});
+        dir_path = dir(initial_path);
+        names_path = {dir_path.name};
+        folders_to_analyse = names_path(endsWith(names_path, 'KV'));
+        data_path = cell(length(folders_to_analyse),1);
+        for i_folder = 1:length(folders_to_analyse)
+            data_path{i_folder} = os.path.join(initial_path, folders_to_analyse{i_folder});
+        end
+        
+        if length(data_path) > 1
+            unique_datapath = 0;
+        else
+            data_path = data_path{1};
+            unique_datapath = 1;
+        end
+    case 'Niels'
+        data_path_root = ['N:\Niels\Igor\'];
+        data_path = os.path.join(data_path_root, recording_date{1},'data');
+        unique_datapath = 1;
+        
+    otherwise
+        keyboard
+end
+if unique_datapath
+    DATA = run_analysis(data_path, experimenter_ID, p, do_plotting, recording_date);
+else
+    DATA = table();
+    for i_folder = 1:length(folders_to_analyse)
+        data = run_analysis(data_path{i_folder}, experimenter_ID, p, do_plotting, recording_date);
+        DATA = [DATA; data];
+    end
+end
+
+%% Write data
+if DATA
+    disp('all done')
+    
+    % disp('saving data')
+    % the_names = unique(DATA.("Date/Cell"));
+    %
+    %
+    % for i_name = 1:length(the_names)
+    %     this_name = the_names{i_name};
+    %     data_to_take = find(ismemberCellRows(DATA.("Date/Cell"), {this_name}));
+    %     this_DATA = DATA(data_to_take,:); %#ok<FNDSB>
+    %     filename_xlsx = os.path.join(GC.path_output_EPSP_analysis.(experimenter_ID), [this_name,  '.xlsx']);
+    %     data_fieldnames = fieldnames(this_DATA);
+    %     data_fieldnames(ismember(data_fieldnames, {'Date/Cell', 'trials', 'Properties', 'Row', 'Variables'})) = [];
+    %     % Loop through trials
+    %     trials = this_DATA.trials;
+    %     n_trials = length(trials);
+    %     for i_trial = 1:n_trials
+    %         this_trial = trials{i_trial};
+    %         sheet_name = this_trial;
+    %         idx = ismember(this_DATA.trials,{this_trial});
+    %         sw_id = this_DATA.("Sweep ids"){idx} ;
+    %         amp = this_DATA.Amplitude{idx};
+    %         wd = this_DATA.Width{idx};
+    %         rt = this_DATA.("Rise time"){idx};
+    %         dt = this_DATA.("Decay time"){idx};
+    %         on = this_DATA.Onset{idx};
+    %         slp = this_DATA.Slope{idx};
+    %         Ri = this_DATA.("Input Resistance (MOhm)"){idx};
+    %         Vm = this_DATA.("Membrane potential"){idx};
+    %         % convert back to Table
+    %         this_table = array2table([sw_id,amp, slp, Ri, Vm, on, wd, rt, dt], 'VariableNames', data_fieldnames);
+    %
+    %         %% Write to Excel
+    %         if exist(filename_xlsx, 'file') && ~overwrite
+    %              writetable(this_table,filename_xlsx,'Sheet',sheet_name, 'Range', 'B1')
+    % %             keyboard
+    % %             original = readtable(filename_xlsx);
+    % %             sz_or = height(original);
+    % %             range1 = ['B',char(num2str(sz_or+5))];
+    % %             writetable(this_table,filename_xlsx,'Sheet',sheet_name, 'Range', range1)
+    %
+    %         elseif ~exist(filename_xlsx, 'file')
+    %
+    %             if overwrite
+    %                 delete(filename_xlsx)
+    %                 writetable(this_table,filename_xlsx,'Sheet',sheet_name, 'Range', 'B1')
+    %             else
+    %                 writetable(this_table,filename_xlsx,'Sheet',sheet_name, 'Range', 'B1')
+    %             end
+    %         end
+    %     end
+    %
+    %
+    %
+    % end
+    %% DONE
+    disp('Done!')
+    DONE = 1;
+else
+    keyboard
+end
+
+end
+
+%% Run analysis
+    function done = run_analysis(data_path, experimenter_ID, p, do_plotting, recording_date)
+        global GC
+        data_dir = dir(data_path);
+        % Pick animals
+        ending = '.ibw';
+        names ={data_dir.name};
+        files_in_folder = names(endsWith(names, ending));
+        files_in_folder = natsort(files_in_folder);
+        
+        % Isolate the files to analyze
+        str_exptr = GC.string_file_selection.(experimenter_ID);
+        if strcmp(experimenter_ID, 'Liselot')
+            is_Amp = cell2mat(cellfun(@(x) sum(ismember(x,str_exptr)) == length(str_exptr) && ~endsWith(x, 'outwave.ibw'), files_in_folder, 'UniformOutput', false));
+        elseif strcmp(experimenter_ID, 'Niels')
+            is_Amp = cell2mat(cellfun(@(x) sum(ismember(x,str_exptr)) == length(str_exptr)+1 && ~endsWith(x, 'outwave.ibw'), files_in_folder, 'UniformOutput', false));
+        elseif strcmp(experimenter_ID, 'Kristina')
+            is_Amp = cell2mat(cellfun(@(x) endsWith(x, 'V1.ibw') && ~endsWith(x, 'outwave.ibw'), files_in_folder, 'UniformOutput', false));
+        end
+        files_to_take = files_in_folder(is_Amp);
+        % is_outwave = cell2mat(cellfun(@(x) sum(ismember(x,str_exptr)) == length(str_exptr) && endsWith(x, 'outwave.ibw'), files_in_folder, 'UniformOutput', false));
+        is_outwave = cell2mat(cellfun(@(x) endsWith(x, 'outwave.ibw'), files_in_folder, 'UniformOutput', false));
+        outwaves_files = files_in_folder(is_outwave);
+        outwave_identifier = GC.set_string_outwave_selection.(experimenter_ID);
+        %% Run analysis
+        sweep_ids = cell(0,0);
+        amp_EPSP =  cell(0,0);
+        width_EPSP =  cell(0,0);
+        rest_Vm =  cell(0,0);
+        Area =  cell(0,0);
+        slope_EPSP =  cell(0,0);
+        Ratio =  cell(0,0);
+        I_sweeps_EPSP =  cell(0,0);
+        decaytime_EPSP =  cell(0,0);
+        % do_plotting = 0;
+        names = cell(length(files_to_take),1);
+        for i_exp = 1:length(files_to_take)
+            try
+                this_exp = files_to_take{i_exp};
+                file_to_read = os.path.join(data_path,this_exp);
+                D = IBWread(file_to_read); % read Voltage traces
+                str_V = strsplit(this_exp,str_exptr);% first characters of the voltage filethat need to match to the Current file
+                start_str_v = [str_V{1}, outwave_identifier] ;
+                outwave_file_start= char(outwaves_files(startsWith(outwaves_files, start_str_v)));
+                % check for correct identifier
+                if isempty(outwave_file_start)
+                    start_str_v = [str_V{1}, str_exptr] ;
+                    outwave_file_start= char(outwaves_files(startsWith(outwaves_files, start_str_v)));
+                end
+                outwave_file = os.path.join(data_path,outwave_file_start);
+                O =  IBWread(outwave_file); % read Current traces
+                I_traces = O.y;
+                data = D.y;
+                
+                [sweep_id, amp, I_sweeps, ~, ~, ~, rat, vm, ar] = Analysis_workflow.EPSP_summation_analysis(experimenter_ID,  data,I_traces, do_plotting, this_exp, p);
+                % Output for all experiments on this folder
+                
+%                 sweep_ids(i_exp) = {sweep_id};
+%                 amp_EPSP(i_exp) = {amp};
+% %                 width_EPSP(i_exp) = {wd} ;
+%                 rest_Vm(i_exp) = {vm};
+%                 Area(i_exp) = {ar};
+% %                 slope_EPSP(i_exp) = {slope};
+%                 Ratio(i_exp) = {rat};
+%                 I_sweeps_EPSP(i_exp) = {I_sweeps};
+% %                 decaytime_EPSP(i_exp) = {decay};
+%                 
+% %                 names(i_exp) = {this_exp};
+                %%
+                
+%                 names_idx = cellfun(@(x) ~isempty(x), names);
+                NAMES = this_exp;
+                
+                
+                seps = strsplit(data_path, '\');
+                this_folder = (seps{end});
+                if strcmp(this_folder, 'data') % For niels' data was something weird.
+                    this_folder = recording_date{1};
+                end
+                
+                if isempty(NAMES)
+                    %             keyboard
+                    disp(['File/cell: ', this_folder, ' has no trials'])
+                    this_table = [];
+                    return
+                end
+                % Name_excel
+                name_excel = this_folder;
+%                 var_names = {'Sweeps ID', 'Amplitude(pA)', 'Amplitude(mV)', 'Vm', 'Ratio', 'Area(ms*mV)'};
+                
+                this_table = table(sweep_id, I_sweeps, amp, vm, rat, ar);
+                
+                
+                
+%                 this_date_table = table(repmat({this_folder}, size(sweep_id,2),1), 'VariableNames', {'Date/Cell'});
+%                 % Name sheet
+                name_sheet = NAMES;
+% %                 names_table = cell2table(NAMES, 'VariableNames', {'trials'});
+%                 Sweeps_table = array2table(sweep_id, 'VariableNames',{'Sweep ids'});
+%                 Amp_table = array2table(amp, 'VariableNames', {'Amplitude'});
+%                 Ratiotable = array2table(rat, 'VariableNames',{'Ratio'});
+%                 I_Amp_table = array2table(I_sweeps, 'VariableNames', {'Inj_Amplitude'});
+%                 Vm_table = array2table(vm, 'VariableNames', {'Vm'});
+%                 Area_table = array2table(ar, 'VariableNames', {'Area (ms*mV)'});
+%              
+%                 this_table = [ Sweeps_table,I_Amp_table, Amp_table,  Vm_table, Ratiotable, Area_table];
+                filepath = os.path.join(GC.path_putput_AP_analysis.(experimenter_ID),'Summation_Analyis');
+                
+                
+                if ~exist(filepath, 'dir')
+                    mkdir(filepath)
+                end
+                filename_xlsx = os.path.join(filepath,  [name_excel,'.xlsx']);
+                
+                writetable(this_table,filename_xlsx,'Sheet',name_sheet, 'Range', 'B1')
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+            catch ME
+%                 disp(ME.message)
+                continue
+            end
+            
+        end
+        
+        done = 1;
+    end
+
+
+% Helper functions
+function puls_str = pulses2str(pulses)
+pulses_str = cell(0,0);
+for i_p = 1:length(pulses)
+    this_pulse = pulses(i_p);
+    pulses_str{i_p} = mat2str(this_pulse);
+end
+puls_str = pulses_str;
+end
+%%
+%#ok<*AGROW>
